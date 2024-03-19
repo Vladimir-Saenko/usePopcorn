cdb035a06f60030c4233d773f0a9f1e713008735
Vladimir Saenko <siborg.451@gmail.com>
2024-03-19 22:19:17 +0700

    part 2 chapter 12-148

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
-//import App from "./components/App";
+import "./index.css";
+import App from "./components/App-copy";
 import StarRating from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} />
+    <App />
+    {/*   <StarRating maxRating={10} defaultRating={5} />
+    <StarRating maxRating={5} size="45" color="darkblue" />*/}
   </React.StrictMode>
 );
